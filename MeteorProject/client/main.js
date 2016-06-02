655c8ca13d44a22e6ca79e39d0cdae2a431c1b96
dongryuck <endfurshyh@gmail.com>
2016-06-02 12:17:06 +0900

    Router
    라우터 연습

---
+++ MeteorProject/client/main.js
@@ -0,0 +1,11 @@
+Router.configure({
+  layoutTemplate: "layout",
+  loadingTemplate: "loading",
+  notFoundTemplate: "notFound"
+});
+
+Router.route("/",{template: "home"});
+Router.route("/Write",{template: "Write"});
+Router.route("/MyCard",{template: "MyCard"});
+Router.route("/Search",{template: "Search"});
+Router.route("/posts/: _id",{name: "posts"});
